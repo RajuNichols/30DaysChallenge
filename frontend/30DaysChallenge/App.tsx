e80378c15a5d2aa8de7540bc05789ae93ceb84aa
RajuNichols <nichols.raju@gmail.com>
2023-01-18 23:06:14 -0600

    CAP-6 Added Fonts, Loading, ReactNativePaper, and Title

--- frontend/30DaysChallenge/App.tsx
+++ frontend/30DaysChallenge/App.tsx
@@ -1,20 +1,37 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, View, Text } from "react-native";
+import { useFonts, Inter_900Black } from "@expo-google-fonts/inter";
+import AppLoading from "expo-app-loading";
 
 export default function App() {
+  let [fontsLoaded, error] = useFonts({
+    Inter_900Black,
+  });
+  if (fontsLoaded) {
+    <AppLoading />;
+  }
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.text}>30 Days Challenge</Text>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#123166",
+    width: "100%",
+    height: "100%",
+    justifyContent: "center",
+    display: "flex",
+  },
+  text: {
+    fontFamily: "Inter_900Black",
+    color: "white",
+    fontSize: 30,
+    textAlign: "center",
+    width: "100%",
+    height: 300,
+    position: "absolute",
   },
 });

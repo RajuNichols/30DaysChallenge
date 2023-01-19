b5034864891fe8f2e2371dbbe54ae2736441d307
RajuNichols <nichols.raju@gmail.com>
2023-01-19 16:46:30 -0600

    CAP-6 Added TextInput, Login, and Register

--- frontend/30DaysChallenge/App.tsx
+++ frontend/30DaysChallenge/App.tsx
@@ -1,19 +1,78 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, View, Text } from "react-native";
-import { useFonts, Inter_900Black } from "@expo-google-fonts/inter";
-import AppLoading from "expo-app-loading";
+import {
+  StyleSheet,
+  View,
+  Text,
+  TextInput,
+  SafeAreaView,
+  TouchableWithoutFeedback,
+  Keyboard,
+  TouchableOpacity,
+} from "react-native";
+import {
+  useFonts,
+  Inter_900Black,
+  Inter_800ExtraBold,
+  Inter_400Regular,
+} from "@expo-google-fonts/inter";
+import React, { useEffect, useCallback, ReactNode } from "react";
+import * as SplashScreen from "expo-splash-screen";
+// import { Button } from "react-native-paper";
 
+SplashScreen.preventAutoHideAsync();
+
+interface Props {
+  children?: ReactNode;
+}
+const DismissKeyboard = ({ children }: Props) => (
+  <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
+    {children}
+  </TouchableWithoutFeedback>
+);
 export default function App() {
   let [fontsLoaded, error] = useFonts({
     Inter_900Black,
+    Inter_800ExtraBold,
+    Inter_400Regular,
   });
-  if (fontsLoaded) {
-    return <AppLoading />;
+
+  useEffect(() => {
+    async function prepare() {
+      await SplashScreen.preventAutoHideAsync();
+    }
+    prepare();
+  }, []);
+
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded) {
+      await SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+
+  if (!fontsLoaded) {
+    return null;
   }
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>30 Days Challenge</Text>
-    </View>
+    <DismissKeyboard>
+      <SafeAreaView style={styles.container} onLayout={onLayoutRootView}>
+        <Text style={styles.text}>30 Days Challenge</Text>
+        <View style={styles.inputContainer}>
+          <TextInput style={styles.input} placeholder="Username" />
+          <TextInput
+            style={styles.input}
+            placeholder="Password"
+            secureTextEntry={true}
+          />
+        </View>
+        <TouchableOpacity style={styles.button}>
+          <Text style={styles.buttonText}>Login</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.register}>
+          <Text style={styles.registerText}>
+            New to 30 Days Challenge? Click here to register
+          </Text>
+        </TouchableOpacity>
+      </SafeAreaView>
+    </DismissKeyboard>
   );
 }
 
@@ -22,16 +81,61 @@ const styles = StyleSheet.create({
     backgroundColor: "#123166",
     width: "100%",
     height: "100%",
-    justifyContent: "center",
     display: "flex",
   },
   text: {
-    fontFamily: "Inter_900Black",
+    fontFamily: "Inter_800ExtraBold",
     color: "white",
-    fontSize: 30,
+    fontSize: 70,
     textAlign: "center",
+    alignItems: "center",
     width: "100%",
     height: 300,
     position: "absolute",
+    lineHeight: 105,
+    top: 95,
+  },
+  inputContainer: {},
+  input: {
+    height: 44,
+    width: 327,
+    fontFamily: "Inter_400Regular",
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+    top: 439,
+    alignSelf: "center",
+    backgroundColor: "#FFFFFF",
+    borderRadius: 4,
+  },
+  button: {
+    backgroundColor: "#F45D9A",
+    width: 327,
+    height: 44,
+    top: 449,
+    alignSelf: "center",
+    borderRadius: 4,
+    justifyContent: "center",
+  },
+  buttonText: {
+    color: "#FFFFFF",
+    fontFamily: "Inter_400Regular",
+    alignSelf: "center",
+    fontSize: 17,
+  },
+  register: {
+    width: 327,
+    height: 48,
+    top: 459,
+    justifyContent: "center",
+    textAlign: "center",
+  },
+  registerText: {
+    color: "#FFFFFF",
+    fontFamily: "Inter_400Regular",
+    alignSelf: "center",
+    fontSize: 17,
+    textAlign: "center",
+    left: 35
   },
 });

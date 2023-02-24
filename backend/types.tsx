3dd265bfbdab2ace4cd13469793fd20b9a73bb9d
Matthew Whittington <mawh7p@mail.missouri.edu>
2023-02-23 22:08:29 -0600

    Started login and create account functions + fixed some things on Users and Challenge objects

--- backend/types.tsx
+++ backend/types.tsx
@@ -1,28 +1,30 @@
-class User{
-    userID:string;
+export class User{
     username:string;
     userPassword:string;
     challenges:{ [key: string]: Challenges };
+    friends:string[];
     numOfChallenges:number;
  
     constructor(username:string, userPassword:string){
         this.username = username;
         this.userPassword = userPassword;
         this.challenges = {};
+        this.friends = [];
         this.numOfChallenges = 0;
     }
  }
  
  
- class Challenges{
+export class Challenges{
     challengeName:string;
+    challengeToken:string;
     challengeDifficulty:number;
     daysCompleted:boolean[];
     startDate:Date;
     endDate:Date;
     isComplete:boolean;
  
-    constructor(challengeName:string, challengeDifficulty:number){
+    constructor(challengeName:string, challengeDifficulty:number, username:string){
         this.challengeName = challengeName;
         this.challengeDifficulty = challengeDifficulty;
         this.startDate = new Date();
@@ -34,6 +36,8 @@ class User{
         for(i=0; i<30; i++){
             this.daysCompleted[i] = false;
         }
+
+        this.challengeToken=username+challengeName;
     }
  
     public getDayCompleted(index:number): boolean{

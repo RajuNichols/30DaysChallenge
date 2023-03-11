8d2697a48d2ef65e5d28aff69469d65388860cf9
RajuNichols <nichols.raju@gmail.com>
2023-03-10 23:04:48 -0600

    Merge stuff

--- frontend/30DaysChallenge/App.tsx
+++ frontend/30DaysChallenge/App.tsx
@@ -10,7 +10,7 @@ export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator
-        initialRouteName="QuestionnairePage"
+        initialRouteName="LandingPage"
         screenOptions={{ headerShown: false }}
       >
         <Stack.Screen name="LandingPage" component={LandingPage} />

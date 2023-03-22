20dd0fe6e1fa95f6476b2bc80045c2344d4ce114
RajuNichols <93800462+RajuNichols@users.noreply.github.com>
2023-03-21 20:10:41 -0500

    Fixed App again...

--- frontend/30DaysChallenge/App.tsx
+++ frontend/30DaysChallenge/App.tsx
@@ -13,7 +13,7 @@ export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator
-        initialRouteName="UserDashboardPage"
+        initialRouteName="LandingPage"
         screenOptions={{ headerShown: false }}
       >
         <Stack.Screen name="LandingPage" component={LandingPage} />

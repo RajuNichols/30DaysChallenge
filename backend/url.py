09847dc2d941ee12171f4691bfc74497fcd4d2ee
AnushaMishra <60370405+AnushaMishra@users.noreply.github.com>
2023-04-06 19:21:24 -0500

    updated url.py with working code

--- backend/url.py
+++ backend/url.py
@@ -1,4 +1,3 @@
-#import scrapy
 import json
 
 # This block will convert the JSON data to a python list
@@ -16,10 +15,16 @@ def list2url(termlist):
   term = ""
   for row in termlist:
     for term in row:
+
+      #keeps the tag term in the list for further processing, ignore it for url creation
+      if(term.startswith("/")):
+        outlist.append(term)
+        continue
+        
       outlist.append("https://pubmed.ncbi.nlm.nih.gov/?term=" + term.replace(" ","+") + "+pmc+open+access%5Bfilter%5D")
   return outlist
 
-ourList2 = load2d("effectshalf.txt")
+ourList2 = load2d("drugsgen.txt")
 
 scrapyList = list2url(ourList2)
 print(scrapyList)

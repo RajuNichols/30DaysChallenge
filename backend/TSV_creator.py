7e30011a9b95a58d5a425a2748afc0fae4027bab
Caleb Wilcox <crwilcox57@gmail.com>
2023-05-01 15:08:39 -0500

    This should work for making TSV's
    Uses sumy, cleans up the sumy output a bit, makes a TSV out of it. Manually change the filename each time you run it, it'll overwrite the output file when run as well so be aware of that.

---
+++ backend/TSV_creator.py
@@ -0,0 +1,71 @@
+# Make sure sumy is installed :)
+# Uncomment the prints if you want output showing the data but it should be pretty solid for any picklefile you have
+import pickle
+from pathlib import Path
+import re
+import sumy
+from sumy.parsers.plaintext import PlaintextParser
+from sumy.nlp.tokenizers import Tokenizer
+from sumy.summarizers.lex_rank import LexRankSummarizer
+import nltk
+nltk.download('punkt')
+
+
+#preprocessing
+def clean_data(data):
+  text = re.sub(r"\[[0-9]*\]"," ",data)
+  text = text.lower()
+  text = re.sub(r'\s+'," ",text)
+  text = re.sub(r","," ",text)
+  return text
+
+
+available = Path("pickle1.txt")
+if(available.is_file()):
+  file = open("pickle1.txt", "rb")
+  labels = pickle.load(file)
+  file.close()
+
+summaries = {}
+
+# Using the positive labeled ones only
+for row in labels:
+  # print(row + ", " + labels[row][0][8:])
+  if (labels[row][1] == "+"):
+    summaries[row] = (labels[row][0][8:])
+    print(row + " added!!\n")
+
+tmp = ""
+proc_summaries = {}
+
+# Go through the summaries and take out the possible ||| in them, capitalize first word, strip blank space
+for row in summaries:
+  # print(summaries[row]+"\nAnd then here is that summarized:\n\n")
+  clean_this_one = clean_data(summaries[row])
+  parser = PlaintextParser.from_string(clean_this_one,Tokenizer("english"))
+  summarizer = LexRankSummarizer()
+  #Summarize the document with 2 sentences
+  summary = summarizer(parser.document, 15)
+  tmp = ""
+  for sentence in summary:
+    if str(sentence)[0] == '|':
+      # print(str(sentence)[12:].strip().capitalize())
+      tmp += str(sentence)[12:].strip().capitalize() + " "
+      # print("\nAvoided 1 ID\n")
+    else:
+      # print(str(sentence).strip().capitalize())
+      tmp += str(sentence).strip().capitalize() + " "
+    # print(type(sentence))
+  proc_summaries[row] = (tmp)
+  print()
+
+f = open("outputTest.txt", "w")
+for row in labels:
+  if (labels[row][1] == "+"):
+    print(row + "\t" + labels[row][2] + "\t" + labels[row][3] + "\t" + proc_summaries[row] + "\n")
+    # print(labels[row][2])
+    # print(labels[row][3])
+    # print(proc_summaries[row]+"\n\n")
+    f.write(row + "\t" + labels[row][2] + "\t" + labels[row][3] + "\t" + proc_summaries[row] + "\n")
+
+f.close()

a7890520ecb958bfc9eff5d1412ff945766168ff
AnushaMishra <60370405+AnushaMishra@users.noreply.github.com>
2023-05-08 23:52:24 -0500

    Updated svmconcl.py
    test set...stratified k fold...stratified split

--- backend/svmconcl.py
+++ backend/svmconcl.py
@@ -6,13 +6,14 @@ from sklearn.model_selection import GridSearchCV, StratifiedKFold
 import pandas as pd
 import re
 from nltk.corpus import stopwords
+from sklearn.model_selection import train_test_split, cross_val_predict
 from sklearn.metrics import precision_score, classification_report
+from nltk.tokenize import word_tokenize
 import numpy as np
-from sklearn.model_selection import cross_val_predict
 
 # Load the data
 data = pd.read_csv('comb.txt', sep='\t', names=['id', 'title','label', 'category', 'body', 'link'])
-data.drop(columns=['id','link','category'], inplace=True)
+data.drop(columns=['link','category'], inplace=True)
 
 print(data['body'], data['title'])
 data['body'] = data['body'].transpose()
@@ -30,6 +31,7 @@ extra_punct = [',', '.', '"', ':', ')', '(', '!', '?', '|', ';', "'", '$', '&',
                '，', '♪', '╩', '╚', '³', '・', '╦', '╣', '╔', '╗', '▬', '❤', 'ï', 'Ø',
                '¹', '≤', '‡', '√', '«', '»', '´', 'º', '¾', '¡', '§', '£', '₤']
 stopword_list = stopwords.words('english') + extra_punct
+
 def preprocess(text):
     # Lowercase
     text = text.lower()
@@ -42,6 +44,40 @@ def preprocess(text):
 # Preprocess the data
 data['title'] = data['title'].apply(preprocess)
 data['body'] = data['body'].apply(preprocess)
+
+def remove_stopwords(text):
+    words = word_tokenize(text)
+    output = []
+    for word in words:
+        if word not in stopword_list:
+            output.append(word)
+    return " ".join(output)
+
+def clean_data(data):
+    text = data.lower()
+    text = re.sub(r'\s+'," ",text)
+    text = re.sub(r","," ",text)
+    text = remove_stopwords(text)
+    return text
+
+full = {}
+if not data.empty:
+    for i, row in data.iterrows():
+        temp = {"feature1": row["title"], "feature2": row["body"]}
+        # full[row["id"]] = (temp, row["label"])
+        temp1 = clean_data(row["title"])
+        temp2 = clean_data(row["body"])
+        full[row["id"]] = ({"feature1":temp1,"feature2":temp2}, row["label"])
+
+X = []
+y = []
+
+for key in full:
+    X.append(full[key][0]["feature1"] + " " + full[key][0]["feature2"])
+    y.append(full[key][1])
+
+# Split the data into training and testing sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42, stratify=y)
 
 # Define the pipeline
 pipeline = Pipeline([
@@ -67,34 +103,20 @@ scoring = {'accuracy': 'accuracy',
 # X = data[['body', 'title']]
 # y = data['label']
 # Perform grid search
-cv = StratifiedKFold(n_splits=5, shuffle=True, random_state=42)
-grid_search = GridSearchCV(pipeline, parameters, cv=cv, n_jobs=-1, scoring='f1_micro', return_train_score=True)
-grid_search.fit(data['title'] + ' ' + data['body'], data['label'])
+cv = StratifiedKFold(n_splits=14, shuffle=True, random_state=42)
+grid_search = GridSearchCV(pipeline, parameters, cv=cv, n_jobs=-1, return_train_score=True)
+grid_search.fit(X_train, y_train)
 
-# y_pred = cross_val_predict(grid_search, data['title'] + ' ' + data['body'], data['label'], cv=cv)
+# Print the best hyperparameters and accuracy
+print("Best parameters:", grid_search.best_params_)
+print("Best accuracy:", grid_search.best_score_)
 
-# # Generate classification report
-# print(classification_report(data['label'], y_pred))
-# Print the best parameters and accuracy
-# Print the best parameters and evaluation metrics
-print("Best parameters: ", grid_search.best_params_)
-print("Best accuracy: ", grid_search.best_score_)
-# best_precision = grid_search.cv_results_['mean_test_precision_macro'][grid_search.best_index_]
-# if np.isnan(best_precision):
-#     best_precision = 0
-# print("Best precision: ", best_precision)
-# best_recall = grid_search.cv_results_['mean_test_recall_macro'][grid_search.best_index_]
-# if np.isnan(best_recall):
-#     best_recall = 0
-# print("Best recall: ", best_recall)
-# best_f1 = grid_search.cv_results_['mean_test_f1_macro'][grid_search.best_index_]
-# if np.isnan(best_f1):
-#     best_f1 = 0
-# print("Best f1-score: ", best_f1)
+# Get the best model
+best_model = grid_search.best_estimator_
 
-# grid_search = GridSearchCV(pipeline, parameters, cv=10, n_jobs=-1)
-# grid_search.fit(data['body'], data['label'])
+# Make predictions on the test data using the best model
+y_pred = best_model.predict(X_test)
 
-# # Print the best parameters and accuracy
-# print("Best parameters: ", grid_search.best_params_)
-# print("Best accuracy: ", grid_search.best_score_)
+# Print the classification report
+print(classification_report(y_test, y_pred))
+print("Test accuracy: ", accuracy_score(y_test, y_pred))
